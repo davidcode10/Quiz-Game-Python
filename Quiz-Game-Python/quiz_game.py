223dacdabc8b4f3a7954bec8972f2b221e598143
davidcode10 <davidedson2030@outlook.com>
2022-02-21 17:53:09 +0100

    first question implemented

--- Quiz-Game-Python/quiz_game.py
+++ Quiz-Game-Python/quiz_game.py
@@ -4,3 +4,9 @@ playing = input('Do you want to play? ')
 
 if playing != 'yes':
     quit()
+
+print("Okay! Le's play :)")
+
+answer = input('What does CPU stand for? ')
+if answer == 'central processing unit':
+    print('Correct!')
